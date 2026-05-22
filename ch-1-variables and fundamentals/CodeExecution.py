efdf439f3e8e14951464f8503f4d709fce4e6e1a
Manish Kumawat <manishkmt211@gmail.com>
2026-05-22 12:31:29 +0530

    Initial commit

--- ch-1-variables and fundamentals/CodeExecution.py
+++ ch-1-variables and fundamentals/CodeExecution.py
@@ -1,6 +1,35 @@
-name = "Manish Kumawat"
-Age = 19
+# name = "Manish Kumawat"
+# age = 19
 
-print(name)
+# print(name)
 
-print(id(name))
+# print(id(name))
+
+# print("Actual Value:",age)
+
+
+#?Taking user input
+#*--> Use input90 to get user input
+
+#*--> Example:
+#*         name = input('Enter your name: ')
+#*         print('Hello', name)
+#!input() alwaye returns string
+# 
+#*-->Convert it before performing calculations    
+
+# name = input("Enter your name:") 
+
+
+# age = input("Enter your age:")
+# print(name) 
+# print(age)
+
+
+#! Indentation
+#*--> Indentation in python is meaningful. You cannot indent randomly like this:
+#*       name = "Manish"
+#*       print(name)
+
+
+#?Take diameter as input and claculate the area of a circle
