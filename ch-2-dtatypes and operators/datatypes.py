a24810bab568afe027ab516814947c643f3df574
Manish Kumawat <manishkmt211@gmail.com>
2026-05-26 15:04:09 +0530

    Initial commit

--- ch-2-dtatypes and operators/datatypes.py
+++ ch-2-dtatypes and operators/datatypes.py
@@ -21,9 +21,9 @@
 
 #!program to take age as input and print value entered and its datatype
 
-age = input("Enter the age:")
-print("Age is",age)
-print("Datatype of age is",type(age))
+# age = input("Enter the age:")
+# print("Age is",age)
+# print("Datatype of age is",type(age))
 
 
 #!Keywords
@@ -39,3 +39,58 @@ print("Datatype of age is",type(age))
             # 31.while    32.with   33.yield
 
             #!Use help("keywords") in python shell to list all current keywords.
+
+
+#!Syntax:-
+            #?Syntax refers to the set of rules that defines how a python program is structured and written so thst the python intrepreter can understand and execute it correctly
+
+
+# name = "manish"
+
+
+
+#! Input two num and sum
+# input1 = int(input("Enter first num:"))
+# input2 = int(input("Enter second num:"))
+
+# sum = input1 + input2
+
+# print(sum)
+
+
+#!Type Conversion:-
+
+#?--> convert the datatype from one to another
+
+
+#!Types:-
+#?-->1.Implicit--> automatically Coberts the small datatype to larger to prevent data loss.
+
+# x = 5
+# y = 5.5
+# z = x + y
+# print(type(x))
+ #*2.Explicit-->Manually converts datatypes using built in function.
+  #  x = "10"
+  #  y = int(x)
+
+
+#!Operators in python
+#?--> Operators perform operations on variables and values.
+#?example:Airthmetic,comparsion, logical
+
+x = 5
+y = 7
+print(x==y)
+print(x<y)
+print(x>y)
+
+print(x>y and x<y )
+
+x = 5
+y = 7
+
+
+print(x>y or x<y )
+
+print(not (x<y) )
