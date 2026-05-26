de589df818ede1c3c15e601cac51db932cef2007
Manish Kumawat <manishkmt211@gmail.com>
2026-05-26 15:16:13 +0530

    Initial commit

---
+++ ch-2-dtatypes and operators/assignment.py
@@ -0,0 +1,12 @@
+
+#!Smart temreture convertor
+
+celsious = float(input("Enter the temp in celsious: "))
+
+farenhite = (celsious * 9/5 + 32)
+
+kelvin = celsious + 273
+
+print("the temp in farenhite is",farenhite)
+
+print("The temp in kelvin is:",kelvin)
