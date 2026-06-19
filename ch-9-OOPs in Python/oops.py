5bfe622be819df24196d68111ef0057c1f5fdc30
Manish Kumawat <manishkmt211@gmail.com>
2026-06-19 13:08:03 +0530

    Initial commit

--- ch-9-OOPs in Python/oops.py
+++ ch-9-OOPs in Python/oops.py
@@ -22,12 +22,58 @@
 
 # ? class creation:-
 
-class vehicle:
-  # Attributes
-  color = "Black"
-  milage = 15
-  cost = 5000000
+# class vehicle:
+#   # Attributes
+#   color = "Black"
+#   milage = 15
+#   cost = 5000000
 
 # object creation
-car = vehicle()
-print(car.color)
+# car = vehicle()
+# print(car.color)
+
+
+# ? Instance Attributes vs class Attributes:-
+
+# * Class Attributes shared by all objects
+
+# class student:
+#   college = "xyz"
+
+# * Instance Attributes is unique for each object.
+
+# class student:
+#   college = "xyz"
+
+
+  # ! __init__() Constructor:-
+# ?                            The __init__() function runs automatically whenever an object is created. It used to initialize attributes.
+# class student:
+#   name = "Manish Kumawat"
+
+#   def __init__(self,name1):
+#     print("whenever a object is created then init constructor called automatically")
+#     self.name1 = name1
+    
+
+# student1 = student("Subash Kumawat") # init method called
+# print(student1.name1)
+# print(student1.name,"\n")
+# student2 = student("Khusharth Sharma") # init method called
+
+# print(student2.name1)
+
+
+# ! Methods (functions inside class):-
+# ?                                   Methods define what an object can do.
+
+# Example:-
+class student:
+  def __init__(self, name):
+    self.name = name
+
+  def hello(self):
+    print("Hello", self.name)
+
+s1 = student("Manish Kumawat")
+s1.hello()
